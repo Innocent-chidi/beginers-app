a6e0c16bb9ac0b9424721233e840e9b453404969
innocent-chidi <innocentchidi10@gmail.com>
2024-07-08 17:49:01 +0100

    setup the project and routes

---
+++ src/components/Header.js
@@ -0,0 +1,9 @@
+import "../css/Header.css"
+
+const Header = () => {
+  return (
+    <div>Header</div>
+  )
+}
+
+export default Header
---
+++ src/components/Hero.js
@@ -0,0 +1,9 @@
+import "../css/Hero.css"
+
+const Hero = () => {
+  return (
+    <div>Hero</div>
+  )
+}
+
+export default Hero
